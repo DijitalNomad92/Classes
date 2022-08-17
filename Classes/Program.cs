61c7b8ec19e78beaec6d5e372e7a9a983fa35741
akilbek.ismanov <akilbek.ismanov@ANK5-YZLMORT-03>
2022-08-17 14:21:29 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ogrenci ogrenci = new ogrenci();
+            //ogrenci.id = 5;
+            //ogrenci.adi = "Çağıl";
+            //ogrenci.soyadi = "Alsaç";
+            //ogrenci.adi = "Eyüp"; // değişkeni yenileyebiliriz
+            //Console.WriteLine(ogrenci.id);  ogrenciyi new'leyerek class'ta tanımladıgımız verilere değişken içinde ulaşabiliriz
+
+            //Student student; //declaration
+            //student = new Student();// initiliation  aşağıda aynı işi tek satirda yapıyor
+
+            Student student = new Student();
+            student.SetId(17);
+            student.SetName("Çagıl");
+            student.SureName = "Alsaç";
+
+            Console.WriteLine(student.GetId() + " - " + student.GetName() + " - " + student.SureName);
+        }
+    }
+
+    class ogrenci  // public kullanmak yanlış, herkese açık bi veri oldugundan
+    {
+        //public int id;
+        //public string adi;
+        //public string soyadi;  // field (alan)
+    }
+
+
+    class Student  // doğru kullanımı
+    {
+        int _id;
+        string _name;
+
+        public string SureName { get; set; }  // prop tab tab ile özelliği çagırıyoruz  property
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
---
+++ ClassesEncapsulation/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ClassesEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri()
+            {
+                Adi = "Çağıl",
+                SoyAdi = "Alsaç",
+                ErkekMi = true,
+                kartNo = "1234 4321 5678 8765"
+            };
+
+            Console.WriteLine("Adı Soyadı: " + musteri.tamAdi + "\nCinsiyeti: "
+                + musteri.Cinsiyet + "\n"
+                + "Kart No: " + musteri.kartNo);
+        }
+    }
+
+    class Musteri
+    {
+
+        public string Adi { get; set; }
+
+
+        public string SoyAdi { get; set; }
+
+        string _kartNo;
+        public string kartNo
+        {
+            get
+            {
+                string yeniKartNo = "**** **** **** " + _kartNo.Substring(15);
+                return yeniKartNo;
+            }
+            set
+            {
+                _kartNo = value;
+            }
+        }
+        public bool ErkekMi { get; set; }
+        public string Cinsiyet
+        {
+            get
+            {
+                return ErkekMi ? "Erkek" : "Kadın";
+            }
+        }
+
+        //public string TamAdi => Adi + " " + SoyAdi;  // bu yontem daha kolay sadece get işlemi yapmak istiyorsan
+
+        public string tamAdi
+        {
+            get
+            {
+                return Adi + " " + SoyAdi;
+            }
+        }
+    }
+
+}
